35a9426809eb55c8b26f414aed6f5b6a6e5f2b0e
skateshindler <57732073+skateshindler@users.noreply.github.com>
2020-02-06 16:07:47 -0800

    Add files via upload

---
+++ assets/javascript/script.js
@@ -0,0 +1,63 @@
+//format date to current month day year
+var date = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+
+//current date / time
+$("#currentDay").append(date);
+
+
+
+// current time of day
+
+var timeOfday = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+updatetime();
+
+function updatetime() {
+  var currentTime = moment().format('H');
+  for(var i = 0; i < timeOfday.length; i++) {
+
+    if (parseInt(timeOfday[i]) > currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: #58ce7b");
+
+
+    }
+    else if (parseInt(timeOfday[i]) < currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: lightgray");
+
+    }
+    else if (parseInt(timeOfday[i]) == currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: #fc665e");
+    
+    }
+  }
+}
+// local storage saving in console but not on refresh 
+$(".rowBtn").on("click", function() {
+    var timeOfday = $(this).parent().attr("id");
+    var textContent = $("input").val().trim();
+
+    localStorage.setItem(timeOfday, textContent);
+    // console.log(timeOfday, textContent);
+});
+
+// individual childern and setting to parent
+  $("#9am").children("input").val(localStorage.getItem("9am"));
+
+  $("#10am").children("input").val(localStorage.getItem("10am"));
+
+  $("#11am").children("input").val(localStorage.getItem("11am"));
+
+  $("#12pm").children("input").val(localStorage.getItem("12pm"));
+
+  $("#1pm").children("input").val(localStorage.getItem("1pm"));
+
+  $("#2pm").children("input").val(localStorage.getItem("2pm"));
+
+  $("#3pm").children("input").val(localStorage.getItem("3pm"));
+
+  $("#4pm").children("input").val(localStorage.getItem("4pm"));
+
+  $("#5pm").children("input").val(localStorage.getItem("5pm"));
+
+  //  local storage , not working :(
+    
